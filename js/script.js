d01dab9d8faaf848e1209620808583583f877909
Nieem <nieemcse019@gmail.com>
2024-08-03 23:56:34 +0600

    key matcher done

--- js/script.js
+++ js/script.js
@@ -2,9 +2,33 @@ function changeText(id) {
     id.innerHTML = "Ooops!";
 }
 
-function generatePin(){
-    let pin=0;
-    pin=Math.floor(100000 + Math.random() * 900000);
-    console.log(Math.floor(100000 + Math.random() * 900000));
-    document.getElementById("pin").innerHTML=pin;
-}
+
+
+
+
+// const collection = document.getElementsByClassName(".example");
+//     collection[0].style.backgroundColor = "red";
+
+    // let buttons = document.getElementsByClassName('button');
+    // console.log(buttons);
+    // for (let i = 0; i < buttons.length; i++) {
+    //    // collection[i].style.backgroundColor = "red";
+    //    buttons[i].addEventListener("click", () => {
+    //               console.log(buttons[i].value);
+    //          });
+    //   }
+
+
+      const nodeList = document.querySelectorAll(".button");
+      console.log(nodeList);
+      for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.backgroundColor = "red";
+      }
+
+    //  buttons.forEach((btn) => {
+
+
+    //     btn.addEventListener("click", () => {
+    //          console.log(btn.value);
+    //      });
+    //})
